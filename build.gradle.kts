00ad8194a68710706b83ae3013be9f25ca1c8b5d
LucasAlfare <god-lucas@hotmail.com>
2025-04-12 18:33:37 -0300

    advance `jvmToolchain` version from `8` to `21`

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.lucasalfare.flbinary"
-version = "v1.6"
+version = "v1.7"
 
 repositories {
   mavenCentral()
@@ -20,7 +20,7 @@ tasks.test {
 }
 
 kotlin {
-  jvmToolchain(8)
+  jvmToolchain(21)
 }
 
 publishing {
